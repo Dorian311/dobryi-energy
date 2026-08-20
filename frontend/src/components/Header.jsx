ea2c43acdf56bb68598be600c70fa127a1ec8ea5
Dorian311 <147277718+Dorian311@users.noreply.github.com>
2026-08-20 18:16:51 +0200

    Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -44,7 +44,7 @@ export default function Header() {
             <img
               src="/logo/dobryi-energy.png"
               alt="Dobryi Energy"
-              className="h-16 md:h-20 w-auto"
+              className="h-20 md:h-36 w-auto"
             />
           </Link>
 
